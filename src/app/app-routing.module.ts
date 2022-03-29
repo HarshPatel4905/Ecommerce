4f0ef51246e45b3f50747b4a7f87e4a61bdbac1a
Harsh Patel <harshpatel4905@gmail.com>
2022-03-29 15:33:29 +0530

    add checkout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { ShopCheckoutComponent } from './pages/shop-checkout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'checkout', component:ShopCheckoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
