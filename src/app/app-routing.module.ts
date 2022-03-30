48e52217fff7aae149396e7af31b2ee9ca5513fe
HP <dpatadiya934@rku.ac.in>
2022-03-30 13:46:29 +0530

    Routing all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,15 @@
+import { ShopWishlistComponent } from './pages/shop-wishlist.component';
+import { ShopTermsConditionsPageComponent } from './pages/shop-terms-conditions-page.component';
+import { ShopStandartFormsComponent } from './pages/shop-standart-forms.component';
+import { ShopShoppingCartNullComponent } from './pages/shop-shopping-cart-null.component';
+import { ShopShoppingCartComponent } from './pages/shop-shopping-cart.component';
+import { ShopSearchResultComponent } from './pages/shop-search-result.component';
+import { ShopProductListComponent } from './pages/shop-product-list.component';
+import { ShopPrivacyPolicyComponent } from './pages/shop-privacy-policy.component';
+import { ShopItemComponent } from './pages/shop-item.component';
+import { ShopGoodsCompareComponent } from './pages/shop-goods-compare.component';
+import { ShopFaqComponent } from './pages/shop-faq.component';
+import { ShopContactsComponent } from './pages/shop-contacts.component';
 import { ShopAccountComponent } from './pages/shop-account.component';
 import { ShopAboutComponent } from './pages/shop-about.component';
 import { ShopHomeComponent } from './pages/shop-home.component';
@@ -6,10 +18,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'checkout', component: ShopCheckoutComponent },
+  { path: 'shop-checkout', component: ShopCheckoutComponent },
   { path: '', component: ShopHomeComponent },
   { path: 'about', component: ShopAboutComponent },
   { path: 'account', component: ShopAccountComponent },
+  { path: 'contacts', component: ShopContactsComponent },
+  { path: 'shop-faq', component: ShopFaqComponent },
+  { path: 'shop-goods-compare', component: ShopGoodsCompareComponent },
+  { path: 'shop-item', component: ShopItemComponent },
+  { path: 'shop-privacy-policy', component: ShopPrivacyPolicyComponent },
+  { path: 'shop-product-list', component: ShopProductListComponent },
+  { path: 'shop-search-result', component: ShopSearchResultComponent },
+  { path: 'shop-shopping-cart', component: ShopShoppingCartComponent },
+  { path: 'shop-standart-forms', component: ShopStandartFormsComponent },
+  { path: 'shop-shopping-cart-null', component: ShopShoppingCartNullComponent },
+  {
+    path: 'shop-terms-conditions-page',
+    component: ShopTermsConditionsPageComponent,
+  },
+  { path: 'shop-wishlist', component: ShopWishlistComponent },
 ];
 
 @NgModule({
